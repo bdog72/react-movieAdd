e9245c87338a08215d6f3168b2e6afe6ce7a070f
Brian Bycynski <high5bri@hotmail.com>
2021-11-12 14:28:22 -0500

    Initial commit

---
+++ build/precache-manifest.71ed1282e68da7fd2caf2d0bdc223b6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7067e65de63fb9f4af592080caabf95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d013e233a0b6bbec7a0e",
+    "url": "/static/css/main.d886df1c.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d013e233a0b6bbec7a0e",
+    "url": "/static/js/main.b29ef25f.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
